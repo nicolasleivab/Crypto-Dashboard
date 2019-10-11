85bcce2dd4b5d1e1b7f48b535a98904ab5df84d9
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2019-10-11 14:56:21 +0200

    dispay table function

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
 /* Main js */
 
 const apiKey = {
-    key: "xxxxxxxx" //insert your private api key from coinmarketcap
+    key: "xxxxxxx" //insert your private api key from coinmarketcap
 };
 
-request('GET', 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?' + 'CMC_PRO_API_KEY=' + apiKey.key)
+request('GET', 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?&symbol=BTC,ETH,XRP,USDT,BCH,LTC,EOS,BNB,BSV,XLM&' + 'CMC_PRO_API_KEY=' + apiKey.key)
 .then((response) => {
     const data = JSON.parse(response.target.responseText)
-    console.log(data);
+    console.log(data.data);
+    const add = data.data;
+    const test = [add.BTC, add.ETH, add.XRP, add.USDT, add.BCH, add.LTC, add.EOS, add.BNB, add.BSV, add.XLM];//array of objects with crypto
+    
+    function displayCryptoBoard() {
+        let theExport = ""; //initialize the export
+        test.forEach((crypto) => theExport += '<tr><td>' + crypto.cmc_rank + '</td><td>' + crypto.name + '</td><td>' + crypto.quote.USD.price+ '</td></tr>'); //prints the row tables
+        document.getElementById("crypto-table").innerHTML = theExport;
+    }
+
+displayCryptoBoard(test); //call table function with the array and append to #crypto-table
+
 }).catch()
 
 function request(method, url) {
@@ -19,4 +30,5 @@ function request(method, url) {
         xhr.send();
 
     });
+    
 }
