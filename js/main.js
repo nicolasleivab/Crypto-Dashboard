2795156e805547d923e68bd5208d0b83b804241f
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2019-10-12 19:26:47 +0200

    coincap api

--- js/main.js
+++ js/main.js
@@ -1,23 +1,18 @@
 /* Main js */
 
-const apiKey = {
-    key: "xxxxxxx" //insert your private api key from coinmarketcap
-};
-
-request('GET', 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?&symbol=BTC,ETH,XRP,USDT,BCH,LTC,EOS,BNB,BSV,XLM&' + 'CMC_PRO_API_KEY=' + apiKey.key)
+request('GET', 'https://api.coincap.io/v2/assets/')
 .then((response) => {
     const data = JSON.parse(response.target.responseText)
-    console.log(data.data);
-    const add = data.data;
-    const test = [add.BTC, add.ETH, add.XRP, add.USDT, add.BCH, add.LTC, add.EOS, add.BNB, add.BSV, add.XLM];//array of objects with crypto
-    
-    function displayCryptoBoard() {
-        let theExport = ""; //initialize the export
-        test.forEach((crypto) => theExport += '<tr><td>' + crypto.cmc_rank + '</td><td>' + crypto.name + '</td><td>' + crypto.quote.USD.price+ '</td></tr>'); //prints the row tables
-        document.getElementById("crypto-table").innerHTML = theExport;
-    }
+    console.log(data);
+    const cryptoData = data.data;
+        
+function displayCryptoBoard(arr) {
+    let theExport = ""; //initialize the export
+    arr.forEach((crypto) => theExport += '<tr><td>' + crypto.rank + '</td><td>' + crypto.name + '</td><td>' + crypto.priceUsd+ '</td></tr>'); //prints the row tables
+    document.getElementById("crypto-table").innerHTML = theExport;
+}
 
-displayCryptoBoard(test); //call table function with the array and append to #crypto-table
+displayCryptoBoard(cryptoData); //call table function with the array and append to #crypto-table
 
 }).catch()
 
