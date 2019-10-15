08dc5ab21f4837b5dbdc74406cf491653ef7db61
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2019-10-15 20:56:45 +0200

    format 24hr price data

--- js/main.js
+++ js/main.js
@@ -7,6 +7,12 @@ request('GET', 'https://api.coincap.io/v2/candles?exchange=binance&interval=m15&
     const btcData = data.data; //array of objects with 15 minutes candles
     console.log(btcData);
     
+    const btcDaily = btcData.slice(41); // 24hr/15min = 96 then 137(fetched data) - 96 = 41
+
+    const btcDailyCloses = btcDaily.map((function(d){return {"price": +d.close, "date": +d.period} ;}));
+    //transform existing array to a new one with the data we need (price and date)
+    console.log(btcDailyCloses);
+
 
 }).catch()
 
