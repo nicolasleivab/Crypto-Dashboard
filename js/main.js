5d8297b63f1bd5f7a27297c96593c53725873a2b
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2019-10-17 14:31:06 +0200

    GET loop

--- js/main.js
+++ js/main.js
@@ -1,45 +1,31 @@
 /* Main js */
 
 //CoinCap API 
-request('GET', 'https://api.coincap.io/v2/candles?exchange=binance&interval=m15&baseId=bitcoin&quoteId=tether')
-.then((response) => {
-    const data = JSON.parse(response.target.responseText);
-    const btcData = data.data; //array of objects with 15 minutes candles
-    console.log(btcData);
-    
-    const btcDaily = btcData.slice(41); // 24hr/15min = 96 then 137(fetched data) - 96 = 41
+const index = ["&baseId=bitcoin&quoteId=tether", "&baseId=ethereum&quoteId=tether", "&baseId=ripple&quoteId=tether", 
+"&baseId=bitcoin-cash&quoteId=tether", "&baseId=litecoin&quoteId=tether", "&baseId=binance-coin&quoteId=tether"];
+const myArray = [];
+const request = new XMLHttpRequest();
 
-    const btcDailyCloses = btcDaily.map((function(d){return {"price": +d.close, "date": new Date(d.period)} ;}));
-    //transform existing array to a new one with the data we need (price and date)
-    console.log(btcDailyCloses);
+(function loop(i, length) {
+    if (i>= length) {
+        return;
+    }
+    const url = "https://api.coincap.io/v2/candles?exchange=binance&interval=m15" + index[i];
 
+    request.open("GET", url);
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE && request.status === 200) { //so we can get the elements in order
+            const data = JSON.parse(request.responseText);
+            const cryptoData = data.data; //array of objects with 15 minutes candles
+            
+            const oneDayData = cryptoData.slice(-96); // 24hr/15min = 96 
+            const formattedData= oneDayData.map((function(d){return {"price": +d.close, "date": new Date(d.period)} ;}));
+            //transform existing array to a new one with the data we need (price and date)
+            console.log(formattedData);
+            myArray.push(formattedData); //storing each GET response to a global array
 
-/* D3 viz */
-let lineChart1 = new LineChart("#chart1", btcDailyCloses, "bitcoin");
-let lineChart2 = new LineChart("#chart2", btcDailyCloses, "bitcoin");
-let lineChart3 = new LineChart("#chart3", btcDailyCloses, "bitcoin");
-let lineChart4 = new LineChart("#chart4", btcDailyCloses, "bitcoin");
-let lineChart5 = new LineChart("#chart5", btcDailyCloses, "bitcoin");
-let lineChart6 = new LineChart("#chart6", btcDailyCloses, "bitcoin");
-
-//calling the  drawChart method for each chart
-lineChart1.drawChart();
-lineChart2.drawChart();
-lineChart3.drawChart();
-lineChart4.drawChart();
-lineChart5.drawChart();
-lineChart6.drawChart();
-
-}).catch()
-
-function request(method, url) {
-    return new Promise(function (resolve, reject){
-        const xhr = new XMLHttpRequest();
-        xhr.open(method, url);
-        xhr.onload = resolve;
-        xhr.onerror = reject;
-        xhr.send();
-
-    });
-    
-}
+            loop(i + 1, length);
+        }
+    }
+    request.send();
+})(0, index.length);
