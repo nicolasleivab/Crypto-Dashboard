efd85075cf2024feb9031a4dbd4fb06b6e5a2999
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2019-10-17 14:11:30 +0200

    mouseover func

--- js/line_chart.js
+++ js/line_chart.js
@@ -16,10 +16,11 @@ class LineChart {
 // set the dimensions of the graph
 const margin = {top: 100, right: 50, bottom: 30, left: 50},
 width = 400 - margin.left - margin.right,
-height = 250 - margin.top - margin.bottom;
+height = 250 - margin.top - margin.bottom,
+vis = this;
 
 // append svg to current parentDiv
-const svg = d3.select(this.parentDiv)
+const svg = d3.select(vis.parentDiv)
 .append("svg")
 .attr("width", width + margin.left + margin.right)
 .attr("height", height + margin.top + margin.bottom)
@@ -27,9 +28,9 @@ const svg = d3.select(this.parentDiv)
 .attr("transform",
       "translate(" + margin.left + "," + margin.top + ")");
 
-// Add x axis, I'm not formatting the date for this viz class
-const x = d3.scaleLinear()
-  .domain(d3.extent(this.formattedData, function(d) { return d.date; }))
+// Add x axis with formatted time
+const x = d3.scaleTime()
+  .domain(d3.extent(vis.formattedData, function(d) { return d.date; }))
   .range([ 0, width ]);
 svg.append("g")
   .attr("transform", "translate(0," + height + ")")
@@ -37,14 +38,14 @@ svg.append("g")
 
 // Add y axis
 const y = d3.scaleLinear()
-  .domain([d3.min(this.formattedData, function(d) { return d.price; }), d3.max(this.formattedData, function(d) { return d.price; })])
+  .domain([d3.min(vis.formattedData, function(d) { return d.price; }), d3.max(vis.formattedData, function(d) { return d.price; })])
   .range([ height, 0 ]);
 svg.append("g")
   .call(d3.axisLeft(y).ticks(5));
 
 // Add the line
 svg.append("path")
-  .datum(this.formattedData)
+  .datum(vis.formattedData)
   .attr("fill", "none")
   .attr("stroke", "blue")
   .attr("stroke-width", 1)
@@ -52,13 +53,54 @@ svg.append("path")
     .x(function(d) { return x(d.date) })
     .y(function(d) { return y(d.price) })
     )
+    .on("mouseover", function() { focus.style("display", null); })
+    .on("mouseout", function() { focus.style("display", "none"); })
+    .on("mousemove", mousemove);
 
 //append this.coin text
 svg.append("text")
   .attr("x", width/2)
   .attr("y", 0)
   .attr("text-anchor", "middle")
-  .text(this.coin)
+  .text(vis.coin);
+
+const focus = svg.append("g")
+  .attr("class", "focus")
+  .style("display", "none");
+
+focus.append("line")
+  .attr("class", "x-hover-line hover-line")
+  .attr("y1", 0)
+  .attr("y2", height);
+
+focus.append("circle")
+.attr("r", 3);
+
+focus.append("text")
+.attr("x", 15)
+  .attr("dy", ".31em");
+
+svg.append("rect")
+.attr("class", "overlay")
+.attr("fill", "transparent")
+.attr("width", width)
+.attr("height", height)
+.on("mouseover", function() { focus.style("display", null); })
+.on("mouseout", function() { focus.style("display", "none"); })
+.on("mousemove", mousemove);
+
+//mousemove function adapted from Adam Janes https://github.com/adamjanes/udemy-d3/blob/master/06/6.10.0/js/main.js
+function mousemove() {
+    console.log(vis.formattedData);
+var x0 = x.invert(d3.mouse(this)[0]),
+  i = d3.bisector(function(d) { return d.date; }).left(vis.formattedData, x0, 1),
+  d0 = vis.formattedData[i - 1],
+  d1 = vis.formattedData[i],
+  d = x0 - d0.date > d1.date - x0 ? d1 : d0;
+focus.attr("transform", "translate(" + x(d.date) + "," + y(d.price) + ")");
+focus.select("text").text(function() { return d.price; });
+focus.select(".x-hover-line").attr("y2", height - y(d.price));
+}
 
     }
 };
--- js/main.js
+++ js/main.js
@@ -3,13 +3,13 @@
 //CoinCap API 
 request('GET', 'https://api.coincap.io/v2/candles?exchange=binance&interval=m15&baseId=bitcoin&quoteId=tether')
 .then((response) => {
-    const data = JSON.parse(response.target.responseText)
+    const data = JSON.parse(response.target.responseText);
     const btcData = data.data; //array of objects with 15 minutes candles
     console.log(btcData);
     
     const btcDaily = btcData.slice(41); // 24hr/15min = 96 then 137(fetched data) - 96 = 41
 
-    const btcDailyCloses = btcDaily.map((function(d){return {"price": +d.close, "date": ((d.period)/1000)} ;}));
+    const btcDailyCloses = btcDaily.map((function(d){return {"price": +d.close, "date": new Date(d.period)} ;}));
     //transform existing array to a new one with the data we need (price and date)
     console.log(btcDailyCloses);
 
