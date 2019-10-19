6fc337ca9dfa26efd83caeaf5cc1a9b480a44fed
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2019-10-19 18:10:03 +0200

    new url

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
 /* Main js */
 
 //CoinCap API 
-const index = ["&baseId=bitcoin&quoteId=tether", "&baseId=ethereum&quoteId=tether", "&baseId=ripple&quoteId=tether", 
-"&baseId=bitcoin-cash&quoteId=tether", "&baseId=litecoin&quoteId=tether", "&baseId=binance-coin&quoteId=tether"],
+const coin = ["bitcoin", "ethereum", "ripple", "tether", "bitcoin-cash", "litecoin"],
     bindsArray = ["lineChart1", "lineChart2", "lineChart3", "lineChart4", "lineChart5", "lineChart6"],
     parentsArray = ["#chart1", "#chart2", "#chart3", "#chart4", "#chart5", "#chart6"],
-    namesArray = ["Bitcoin", "Ethereum", "XRP", "Bitcoin Cash", "Litecoin", "Binance Coin"],
     titlesArray = ["title1", "title2", "title3", "title4", "title5", "title6"],
     request = new XMLHttpRequest();
 
@@ -13,7 +11,7 @@ const index = ["&baseId=bitcoin&quoteId=tether", "&baseId=ethereum&quoteId=tethe
     if (i>= length) {
         return;
     }
-    const url = "https://api.coincap.io/v2/candles?exchange=binance&interval=m15" + index[i];
+    const url = "https://api.coincap.io/v2/assets/"+coin[i]+"/history?interval=m15";
 
     request.open("GET", url);
     request.onreadystatechange = function() {
@@ -22,13 +20,13 @@ const index = ["&baseId=bitcoin&quoteId=tether", "&baseId=ethereum&quoteId=tethe
             const cryptoData = data.data; //array of objects with 15 minutes candles
             
             const oneDayData = cryptoData.slice(-96); // 24hr/15min = 96 
-            const formattedData= oneDayData.map((function(d){return {"price": +d.close, "date": new Date(d.period)} ;}));
+            const formattedData= oneDayData.map((function(d){return {"price": (Math.round(d.priceUsd*10000))/10000, "date": new Date(d.time)} ;}));
             //transform existing array to a new one with the data we need (price and date)
             console.log(formattedData);
             
             /* D3 viz */
             //create a new binding with each loop for each coin
-            bindsArray[i] = new LineChart(parentsArray[i], formattedData, namesArray[i], titlesArray[i]);
+            bindsArray[i] = new LineChart(parentsArray[i], formattedData, coin[i], titlesArray[i]);
 
             //calling the  drawChart method for each coin
             bindsArray[i].drawChart();
@@ -38,4 +36,4 @@ const index = ["&baseId=bitcoin&quoteId=tether", "&baseId=ethereum&quoteId=tethe
         }
     }
     request.send();
-})(0, index.length);
+})(0, coin.length);
