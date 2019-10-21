57f26377b7c7c3570bd5ae194edaad5006e0c572
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2019-10-21 21:16:37 +0200

    mouseover from linechart

--- js/history_chart.js
+++ js/history_chart.js
@@ -44,14 +44,62 @@ svg.append("g")
 svg.append("path")
   .datum(vis.formattedData)
   .attr("fill", "none")
-  .attr("stroke", "blue")
+  .attr("stroke", "#b3b3ff")
   .attr("stroke-width", 1)
   .attr("d", d3.line()
     .x(function(d) { return x(d.date) })
     .y(function(d) { return y(d.price) })
     )
     .on("mouseover", function() { focus.style("display", null); })
-    .on("mouseout", function() { focus.style("display", "none"); });
+    .on("mouseout", function() { focus.style("display", "none"); })
+    .on("mousemove", mousemove);
+
+const focus = svg.append("g")
+  .attr("class", "focus")
+  .style("display", "none");
+
+  //bottom line
+focus.append("line")
+  .attr("class", "x-hover-line hover-line2")
+  .attr("y1", 0)
+  .attr("y2", height);
+
+  //upper line
+focus.append("line")
+  .attr("class", "x2-hover-line hover-line2")
+  .attr("y1", 0)
+  .attr("y2", height);
+
+focus.append("circle")
+.attr("r", 2);
+
+focus.append("text")
+.attr("x", 15)
+  .attr("dy", ".31em");
+
+svg.append("rect")
+.attr("class", "overlay")
+.attr("fill", "transparent")
+.attr("width", width)
+.attr("height", height)
+.on("mouseover", function() { focus.style("display", null); })
+.on("mouseout", function() { focus.style("display", "none"); })
+.on("mousemove", mousemove);
+
+
+//mousemove function adapted from Adam Janes https://github.com/adamjanes/udemy-d3/blob/master/06/6.10.0/js/main.js
+function mousemove() {
+var x0 = x.invert(d3.mouse(this)[0]),
+  i = d3.bisector(function(d) { return d.date; }).left(vis.formattedData, x0, 1),
+  d0 = vis.formattedData[i - 1],
+  d1 = vis.formattedData[i],
+  d = x0 - d0.date > d1.date - x0 ? d1 : d0;
+focus.attr("transform", "translate(" + x(d.date) + "," + y(d.price) + ")");
+focus.select("text").text(d.price);
+focus.select(".x-hover-line").attr("y2", (height - y(d.price)));
+focus.select(".x2-hover-line").attr("y2", (height - y(d.price)) + (- height));
+
+}
 
 //append this.coin text to innetHTML
 document.getElementById("title").innerHTML = vis.coin;
