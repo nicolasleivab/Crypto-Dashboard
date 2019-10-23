48a4c485ba30de7c0b4599334a25f7b970a8ebeb
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2019-10-23 21:38:47 +0200

    defaul chart

--- js/history_chart.js
+++ js/history_chart.js
@@ -1,7 +1,7 @@
 /* D3 History Line Chart */
 
 // set the dimensions of the graph
-const margin = {top: 10, right: 50, bottom: 50, left: 50},
+const margin = {top: 30, right: 50, bottom: 50, left: 50},
 width = 700 - margin.left - margin.right,
 height = 400 - margin.top - margin.bottom;
 
@@ -77,8 +77,6 @@ g.append("rect")
 .on("mouseover", function() { focus.style("display", null); })
 .on("mouseout", function() { focus.style("display", "none"); })
 .on("mousemove", mousemove);
-
-const formatDate = d3.timeFormat("%m/%d %H:%M");
 
 //mousemove function adapted from Adam Janes https://github.com/adamjanes/udemy-d3/blob/master/06/6.10.0/js/main.js
 function mousemove() {
--- js/search.js
+++ js/search.js
@@ -1,6 +1,71 @@
 /* Search js */
 let topCoins = JSON.parse(localStorage.getItem('arrayToPass'));
+let selectedCoin;
+let request = new XMLHttpRequest();
 
+if(selectedCoin != undefined){
+//if selected from dashboard
+let url = "https://api.coincap.io/v2/assets/"+selectedCoin+"/history?interval=m15";
+    request.open("GET", url);
+request.onreadystatechange = function() {
+    if(request.readyState === XMLHttpRequest.DONE && request.status === 200) {
+        let data = JSON.parse(request.responseText);
+        const cryptoData = data.data;
+        const oneDayData = cryptoData.slice(-96); // 24hr/15min = 96 (start with 1 day chart by default) 
+        const formattedData= oneDayData.map((function(d){return {"price": (Math.round(d.priceUsd*10000))/10000, "date": new Date(d.time)} ;}));
+        console.log(formattedData);
+        //call update func to render the chart with new data
+        updateChart(formattedData);
+
+        //append coin text to innetHTML
+        document.getElementById("title").innerHTML = selectedCoin;
+        //filter this coin
+        function filterCoin(coin) {
+            return coin.id == selectedCoin;
+        }
+        
+        let filtered = topCoins.filter(filterCoin);
+        //append coin rank to innetHTML
+         document.getElementById("rank").innerHTML = "Rank #" + filtered[0].rank;
+        
+    } 
+}
+request.send();
+}
+//default chart
+else{
+//Request input coin candles data
+let url = "https://api.coincap.io/v2/assets/bitcoin/history?interval=m15";
+    request.open("GET", url);
+request.onreadystatechange = function() {
+    if(request.readyState === XMLHttpRequest.DONE && request.status === 200) {
+        let data = JSON.parse(request.responseText);
+        const cryptoData = data.data;
+        const oneDayData = cryptoData.slice(-96); // 24hr/15min = 96 (start with 1 day chart by default) 
+        const formattedData= oneDayData.map((function(d){return {"price": (Math.round(d.priceUsd*10000))/10000, "date": new Date(d.time)} ;}));
+        console.log(formattedData);
+
+        //call update func to render the chart with new data
+        updateChart(formattedData);
+
+        //append coin text to innetHTML
+        document.getElementById("title").innerHTML = "bitcoin";
+
+        function filterCoin(coin) {
+            return coin.id == "bitcoin";
+        }
+        
+        let filtered = topCoins.filter(filterCoin);
+
+        //append coin rank to innetHTML
+        document.getElementById("rank").innerHTML = "Rank #" + filtered[0].rank;
+        
+    } 
+}
+request.send();
+
+}
+//selected from search input
 function clicked() {
     let inputCoin = document.getElementById('search').value;
     inputCoin = inputCoin.replace(/\s+/g, '-').toLowerCase();
@@ -9,7 +74,6 @@ function clicked() {
 //Request input coin candles data
 let url = "https://api.coincap.io/v2/assets/"+inputCoin+"/history?interval=m15";
 
-let request = new XMLHttpRequest();
 request.open("GET", url);
 request.onreadystatechange = function() {
     if(request.readyState === XMLHttpRequest.DONE && request.status === 200) {
@@ -20,6 +84,17 @@ request.onreadystatechange = function() {
         console.log(formattedData);
         //call update func to render the chart with new data
         updateChart(formattedData);
+        
+        //append coin text to innetHTML
+        document.getElementById("title").innerHTML = inputCoin;
+        //filter this coin
+        function filterCoin(coin) {
+            return coin.id == inputCoin;
+        }
+        
+        let filtered = topCoins.filter(filterCoin);
+         //append coin rank to innetHTML
+         document.getElementById("rank").innerHTML = "Rank #" + filtered[0].rank;
         
     }
    
