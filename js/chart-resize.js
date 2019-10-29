466748c3f2e37aa4e377f2926ae7ca2f435308f1
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2019-10-29 19:10:13 +0100

    initial portview resize

--- js/chart-resize.js
+++ js/chart-resize.js
@@ -1,14 +1,20 @@
 /* Media Queries js */
 //apply different class on resize for a responsive layout
+const initialW = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+//resize if it starts with smaller device
+if(initialW < 1100){   
+    document.getElementById('chart-data-grid').classList.remove('chart-outter-grid');
+    document.getElementById('chart-data-grid').classList.add('chart-outter-grid2');
+};  
+//change on resize
 function resizeChart(){
-    let w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-    
-    if (w < 1100) {
+let w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+if (w < 1100) {
         document.getElementById('chart-data-grid').classList.remove('chart-outter-grid');
         document.getElementById('chart-data-grid').classList.add('chart-outter-grid2'); 
       }else{
-        document.getElementById('chart-data-grid').classList.add('chart-outter-grid');
-        document.getElementById('chart-data-grid').classList.remove('chart-outter-grid2');  
+        document.getElementById('chart-data-grid').classList.remove('chart-outter-grid2'); 
+        document.getElementById('chart-data-grid').classList.add('chart-outter-grid'); 
     }
-    };
-    window.addEventListener("resize", resizeChart);
+};
+window.addEventListener("resize", resizeChart);
--- js/dashboard-resize.js
+++ js/dashboard-resize.js
@@ -1,14 +1,21 @@
 /* Media Queries js */
 //apply different class on resize for a responsive layout
+const initialW = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+//resize if it starts with smaller device
+if(initialW< 1200){   
+    document.getElementById('dashboard-grid').classList.remove('grid-container');
+    document.getElementById('dashboard-grid').classList.add('grid-container2');
+}
+//change on resize
 function resizeGrid(){
 let w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-
 if (w < 1200) {
     document.getElementById('dashboard-grid').classList.remove('grid-container');
-    document.getElementById('dashboard-grid').classList.add('grid-container2')
+    document.getElementById('dashboard-grid').classList.add('grid-container2');
   }else{
+    document.getElementById('dashboard-grid').classList.remove('grid-container2');
     document.getElementById('dashboard-grid').classList.add('grid-container');
-    document.getElementById('dashboard-grid').classList.remove('grid-container2') 
-};
+    
 }
+};
 window.addEventListener("resize", resizeGrid);
