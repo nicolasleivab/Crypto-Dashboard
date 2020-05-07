5f1e642e89e35d1a73d486b184cd5775bd2e0070
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2020-05-07 07:49:57 -0400

    auth context, NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./components/layout/NavBar";
+import AuthState from "./context/auth/AuthState";
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthState>
+      <div className={styles.App}>
+        <NavBar />
+      </div>
+    </AuthState>
   );
 }
 
---
+++ client/src/components/layout/NavBar.js
@@ -0,0 +1,67 @@
+import React, { useContext, useEffect } from "react";
+import PropTypes from "prop-types";
+import styles from "./NavBar.module.css";
+import ShowChartIcon from "@material-ui/icons/ShowChart";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import AuthContext from "../../context/auth/authContext";
+
+const NavBar = ({ title, icon }) => {
+  const authContext = useContext(AuthContext);
+  const {
+    isAuthenticated,
+    logoutUser,
+    user,
+    loginUser,
+    loadUser,
+  } = authContext;
+
+  const onLogout = () => {
+    logoutUser();
+  };
+
+  const authLinks = (
+    <div className={styles.flexContainer}>
+      <li>Welcome {user && user.name}!</li>
+      <li>
+        <a
+          href="#!"
+          onClick={onLogout}
+          style={{ display: "flex", alignItems: "center" }}
+        >
+          <p className={styles.hide}>Logout</p>
+          <ExitToAppIcon fontSize="large" className={styles.Icon} />
+        </a>
+      </li>
+    </div>
+  );
+
+  const guessLinks = (
+    <div className={styles.flexContainer}>
+      <li>
+        <a href="#">Sign In</a>
+      </li>
+    </div>
+  );
+  useEffect(() => {
+    //loginUser(user);
+  }, []);
+  return (
+    <div className={styles.NavBar}>
+      <div className={styles.titleContainer}>
+        <p className={styles.title}>{title}</p>
+        <ShowChartIcon fontSize="large" style={{ marginLeft: 10 }} />
+      </div>
+      <ul>{isAuthenticated ? authLinks : guessLinks}</ul>
+    </div>
+  );
+};
+
+NavBar.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+NavBar.defaultProps = {
+  title: "Crypto Performance",
+};
+
+export default NavBar;
